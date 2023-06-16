a17d3d52a946b0eaece4d842adcc756b1097ebfe
paulomarinato <marinatopaulo@gmail.com>
2023-06-16 00:10:39 -0300

    conclusão Layout tela Signup, envio da imagem do layout para a pasta de imagen

--- app/src/main/java/com/marinato/listatelefonica/Activity/SignupActivity.kt
+++ app/src/main/java/com/marinato/listatelefonica/Activity/SignupActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.marinato.listatelefonica.R
 
-class SignupActivity2 : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup2)
